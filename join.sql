610fc40c2309c7b9a161b7e563c32cde7175ddbd
rananthan3 <rajan.ananthan@gmail.com>
2018-11-04 08:34:28 -0500

    Delete files

--- join.sql
+++
@@ -1,9 +0,0 @@
-SELECT Order_ID, Product_ID, Quantity, Price, Quantity*Price AS Total FROM OrderItem 
-LEFT JOIN Product 
-ON OrderItem.Product_ID=Product.ID 
-WHERE OrderItem.Order_ID = "419599158";
-
-SELECT SUM(Quantity*Price) AS Total FROM OrderItem 
-LEFT JOIN Product 
-ON OrderItem.Product_ID=Product.ID 
-WHERE OrderItem.Order_ID = "419599158";
